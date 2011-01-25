c21185fcd77d8ca5f0c13aace4ab721d43087d93
cdnicoll <cdnicoll@localhost>
2011-01-25 11:40:22 -0800

    -fixing this branch

--- Tests/Bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . 'lib' . PATH_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'classes');
-date_default_timezone_set('America/Los_Angeles');
-
-require_once '../lib/themewidget/ThemeWidgets.php';
-require_once 'PHPUnit/Framework.php';
-
-require_once '../../../library/Zend/Loader/AutoLoader.php';
-
